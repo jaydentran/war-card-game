ebd80a39ba18a14139c48c7c6f602b955290e3bc
jaydentran <jayden.uow@gmail.com>
2021-02-10 00:28:49 +1100

    first commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Jayden Tran on 7/2/21.
+//  Copyright © 2021 Jayden Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
