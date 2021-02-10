e122642a0d2faded2d01664c0222566434daa722
jaydentran <jayden.uow@gmail.com>
2021-02-10 22:57:03 +1100

    added final code

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -17,11 +17,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var leftScoreLabel: UILabel!
     @IBOutlet weak var rightScoreLabel: UILabel!
     
+    private var playerScore = 0
+    private var bankerScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        let leftNumber = Int.random(in: 2...14)
+        let rightNumber = Int.random(in: 2...14)
+        
+        
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
+        
+        if leftNumber > rightNumber {
+            playerScore += 1
+        } else if rightNumber > leftNumber {
+            bankerScore += 1
+        } else {
+            // nothing
+        }
+        leftScoreLabel.text = String(playerScore)
+        rightScoreLabel.text = String(bankerScore)
+        
+        
+    }
+    
 }
 
